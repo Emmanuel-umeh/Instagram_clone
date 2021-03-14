4b75b832ce78fb760ed797459e166a67c7eab8d8
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-14 20:33:27 +0100

    --- Auto-Git Commit ---

--- components/main/Save.js
+++ components/main/Save.js
@@ -1,42 +1,47 @@
-import { Button, Text, View } from 'native-base'
-import React from 'react'
-import { Image, StyleSheet } from 'react-native'
-import { TextInput } from 'react-native-gesture-handler'
-import { useState } from 'react/cjs/react.development'
-import firebase from "firebase"
-require("firebase/firestore")
-require("firebase/firebase-storage")
+import { Button, Text, View } from "native-base";
+import React from "react";
+import { Image, StyleSheet } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import { useState } from "react/cjs/react.development";
+import firebase from "firebase";
+require("firebase/firestore");
+require("firebase/firebase-storage");
 export default function Save(props) {
-    console.log(props.route.params)
-    const {image} = props.route.params
+  console.log(props.route.params);
+  const { image } = props.route.params;
 
-    const [caption, setCaption] = useState("")
+  const [caption, setCaption] = useState("");
 
-    uploadImage =  async ()=>{
-        const response = await fetch(image)
-    }
-    return (
-     <View style = {styles.container}>
+  uploadImage = async () => {
+    const response = await fetch(image);
+    const blob = await response.blob();
+    const task = firebase.storage().ref().child();
+  };
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri: image,
+        }}
+      />
 
-<Image source = {{
-    uri : image
-}} />
+      <TextInput placeholder="Write a caption. . ." onChangeText={setCaption} />
 
-<TextInput placeholder = "Write a caption. . ." onChangeText ={setCaption} />
-
-<Button block onPress ={()=>{
-    uploadImage()
-}}><Text>Save</Text></Button>
-     </View>
-    )
+      <Button
+        block
+        onPress={() => {
+          uploadImage();
+        }}
+      >
+        <Text>Save</Text>
+      </Button>
+    </View>
+  );
 }
 
-
 const styles = StyleSheet.create({
-    container : {
-        flex : 1
-    },
-    image : {
-
-    }
-})
+  container: {
+    flex: 1,
+  },
+  image: {},
+});
