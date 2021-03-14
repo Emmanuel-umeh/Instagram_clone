e76ab092af98866512608dc76596f13a8f24dca3
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-14 20:28:57 +0100

    --- Auto-Git Commit ---

--- components/main/Save.js
+++ components/main/Save.js
@@ -1,16 +1,22 @@
 import { View } from 'native-base'
 import React from 'react'
 import { Image, StyleSheet } from 'react-native'
+import { TextInput } from 'react-native-gesture-handler'
+import { useState } from 'react/cjs/react.development'
 
 export default function Save(props) {
     console.log(props.route.params)
     const {image} = props.route.params
+
+    const [caption, captionChanged] = useState("")
     return (
      <View style = {styles.container}>
 
 <Image source = {{
     uri : image
 }} />
+
+<TextInput placeholder = "Write a caption. . ." onChangeText ={captionChanged} />
      </View>
     )
 }
