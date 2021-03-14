83b92883f1dba969f5fc457b9bfb92690561af89
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-14 16:23:12 +0100

    --- Auto-Git Commit ---

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -1,12 +1,22 @@
 import { Text, View } from 'native-base'
 import React, { Component } from 'react'
+import { StyleSheet } from 'react-native'
 
 export default class Feed extends Component {
     render() {
         return (
-            <View>
+            <View style = {styles.container}>
                 <Text>On the feed page</Text>
             </View>
         )
     }
 }
+
+
+const styles = StyleSheet.create({
+    container : {
+        flex :1,
+        alignContent : "center",
+        justifyContent : "center"
+    }
+})
