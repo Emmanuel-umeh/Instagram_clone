3acdcfdacb1979c32ed2b4dbb27106e23dd854fd
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-14 16:28:57 +0100

    --- Auto-Git Commit ---

---
+++ components/main/Profile.js
@@ -0,0 +1,22 @@
+import { Text, View } from 'native-base'
+import React, { Component } from 'react'
+import { StyleSheet } from 'react-native'
+
+export default class Profile extends Component {
+    render() {
+        return (
+            <View style = {styles.container}>
+                <Text>On the Profile page</Text>
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container : {
+        flex :1,
+        alignContent : "center",
+        justifyContent : "center"
+    }
+})
