ff1e9b473143069e480775c2258608427c21757f
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-14 16:17:42 +0100

    --- Auto-Git Commit ---

--- components/main/Feed.js
+++ components/main/Feed.js
@@ -1,11 +1,11 @@
-import { View } from 'native-base'
+import { Text, View } from 'native-base'
 import React, { Component } from 'react'
 
 export default class Feed extends Component {
     render() {
         return (
             <View>
-                
+                <Text>On the feed page</Text>
             </View>
         )
     }
