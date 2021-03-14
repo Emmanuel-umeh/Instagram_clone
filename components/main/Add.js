5d2dbb9d70a90d28db9a1046888c26e6c2dd87e6
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-14 18:11:02 +0100

    --- Auto-Git Commit ---

--- components/main/Add.js
+++ components/main/Add.js
@@ -34,6 +34,22 @@ export default function Add() {
   }, []);
 
 
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+
   const takePicture = async() =>{
 if(camera){
     const data = await camera.takePictureAsync(null)
