929d619fa426bd0f3fa6e513af3dcb4ad995a7da
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-14 18:11:47 +0100

    --- Auto-Git Commit ---

--- components/main/Add.js
+++ components/main/Add.js
@@ -36,7 +36,7 @@ export default function Add() {
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
       allowsEditing: true,
       aspect: [1, 1],
       quality: 1,
