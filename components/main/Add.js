ebdec969f2cf2aa0f0404451f16ffafdc27ecbaf
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-14 17:31:59 +0100

    --- Auto-Git Commit ---

--- components/main/Add.js
+++ components/main/Add.js
@@ -35,7 +35,7 @@ export default function Add() {
       
     </View>
     <View style={styles.buttonContainer}>
-<Icon name = "ios-switch"
+<Icon name = "md-switch"
   style={styles.button}
   onPress={() => {
     setType(
