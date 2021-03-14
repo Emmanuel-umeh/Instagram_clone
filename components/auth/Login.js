8d7976a8a4b43f7c77eae934f5cc35b251775ec8
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-14 14:17:06 +0100

    --- Auto-Git Commit ---

---
+++ components/auth/Login.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+
+
+import { Text, View, TextInput, StyleSheet, Button  } from 'react-native'
+import firebase from "firebase"
+export default class Login extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            email : "",
+            password : ""
+
+        }
+    }
+
+    signUp =  async()=>{
+
+        try {
+            const {email, password, name} = this.state
+// if(email.length || password || !name){
+//     console.log(
+// }
+
+var response  =await   firebase.auth.createUserWithEmailAndPassword(email,password)
+
+console.log({response})
+        } catch (error) {
+            console.log({error})
+        }
+
+    }
+    render() {
+        return (
+            <View style = {styles.container}>
+
+<TextInput placeholder = "name" onChangeText ={(name)=>{
+    this.setState({
+        name
+    })
+}}
+
+/>
+<TextInput placeholder = "email" onChangeText ={(email)=>{
+    this.setState({
+        email
+    })
+}}
+
+/>
+<TextInput 
+secureTextEntry = {true}
+ placeholder = "password" onChangeText ={(password)=>{
+    this.setState({
+        password
+    })
+}}
+
+/>
+
+<Button title = "Sign Up" onPress = {this.signUp}></Button>
+            </View>
+        )
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1,
+        justifyContent : "center"
+    }
+})
