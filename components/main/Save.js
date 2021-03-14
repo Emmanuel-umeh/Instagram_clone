b7ed8e3bfa2ab9cb0273619d0756e3e7fb5b3835
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-14 20:24:57 +0100

    --- Auto-Git Commit ---

--- components/main/Save.js
+++ components/main/Save.js
@@ -1,11 +1,12 @@
 import { View } from 'native-base'
 import React from 'react'
 
-export default function Save() {
+export default function Save(props) {
+    console.log(props.route.params)
     return (
      <View>
 
-         
+
      </View>
     )
 }
