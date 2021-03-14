ca002a1a3151fdbc6211232e1e482185f05e225e
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-14 17:05:28 +0100

    --- Auto-Git Commit ---

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,22 +1,41 @@
-import { Text, View } from 'native-base'
-import React, { Component } from 'react'
-import { StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera';
 
-export default class Add extends Component {
-    render() {
-        return (
-            <View style = {styles.container}>
-                <Text>On the Add page</Text>
-            </View>
-        )
-    }
+export default function Add() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={styles.container}>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}>
+            <Text style={styles.text}> Flip </Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </View>
+  );
 }
-
-
-const styles = StyleSheet.create({
-    container : {
-        flex :1,
-        alignContent : "center",
-        justifyContent : "center"
-    }
-})
