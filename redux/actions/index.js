7799e624f37f2485cbc4fd2acd59f8b38f405259
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-14 15:54:55 +0100

    --- Auto-Git Commit ---

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -4,20 +4,23 @@ import {
     USER_STATE_CHANGED
 } from "../constants/index"
 
-export async function  fetchUser(){
+export  function  fetchUser(){
     return((dispatch ) =>{
-    let currentUser  = await   firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid)
-        .get()
+  firebase.firestore().collection('users').doc(firebase.auth().currentUser.uid)
+        .get().then(currentUser =>{
+            if(currentUser.exists){
 
-        if(currentUser.exists){
+                console.log(currentUser.data)
+                dispatch({
+                    type : USER_STATE_CHANGED, 
+                    currentUser : currentUser.data()
+                })
+            }else{
+                console.log("no user exists")
+            }
 
-            console.log(currentUser.data())
-            dispatch({
-                type : USER_STATE_CHANGED, 
-                currentUser : currentUser.data()
-            })
-        }else{
-            console.log("no user exists")
-        }
+        })
+
+     
     })
 }
