1caf119b63fbf3beedae7b5e11b81a8ce0892f2b
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-14 20:26:57 +0100

    --- Auto-Git Commit ---

--- components/main/Save.js
+++ components/main/Save.js
@@ -1,12 +1,26 @@
 import { View } from 'native-base'
 import React from 'react'
+import { Image, StyleSheet } from 'react-native'
 
 export default function Save(props) {
     console.log(props.route.params)
+    const {image} = props.route.params
     return (
-     <View>
+     <View style = {styles.container}>
 
-
+<Image source = {{
+    uri : image
+}} />
      </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    container : {
+        flex : 1
+    },
+    image : {
+
+    }
+})
