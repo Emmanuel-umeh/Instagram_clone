2624115e1cc140d1e4c34498ad1821fe70547bec
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-15 14:02:08 +0100

    --- Auto-Git Commit ---

--- App.js
+++ App.js
@@ -130,7 +130,7 @@ firebase.auth().onAuthStateChanged((user)=>{
             headerShown : false
           }} />
           <Stack.Screen name = "Add" component = {AddScreen} />
-          <Stack.Screen name = "Save" component = {SaveScreen} />
+          <Stack.Screen name = "Save" component = {SaveScreen} navigation = {this.props.navigation} />
           {/* <Stack.Screen name = "Register" component = {RegisterScreen} options = {{
             headerShown : false
           }} />
