b48ba95460b9a943a97821fb0e87b835811604e5
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-15 14:05:23 +0100

    --- Auto-Git Commit ---

--- components/main/Save.js
+++ components/main/Save.js
@@ -5,7 +5,7 @@ import { TextInput } from "react-native-gesture-handler";
 import firebase from "firebase";
 require("firebase/firestore");
 require("firebase/firebase-storage");
-export default function Save(props, {navigation}) {
+export default function Save(props) {
   console.log(props.route.params);
   const { image } = props.route.params;
 
@@ -23,7 +23,7 @@ export default function Save(props, {navigation}) {
       // create date on firebase of creation
       creation : firebase.firestore.FieldValue.serverTimestamp()
     }).then(()=>{
-      navigation.popToTop()
+      props.navigation.popToTop()
     })
   }
 
