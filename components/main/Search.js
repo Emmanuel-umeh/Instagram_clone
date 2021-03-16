c2e63fb1e7659a11cee3b55aba6b138fa8dc2ae1
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-16 20:09:16 +0100

    --- Auto-Git Commit ---

---
+++ components/main/Search.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+import {Text, View,TextInput,FlatList} from "react-native"
+import firebase from "firebase"
+require("firebase/firebase")
+
+export default function Search() {
+    const [users, setUsers] = useState([])
+    return (
+        <div>
+            
+        </div>
+    )
+}
