e52928ef04dce64a286730a87d6646db2efa462d
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-16 20:31:18 +0100

    --- Auto-Git Commit ---

--- components/main/Search.js
+++ components/main/Search.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import {Text, View,TextInput,FlatList} from "react-native"
 import { Container, Header, Content, List, ListItem } from 'native-base';
 import firebase from "firebase"
-require("firebase/firebase")
+require("firebase/firestore")
 
 export default function Search() {
     const [users, setUsers] = useState([])
@@ -20,23 +20,22 @@ export default function Search() {
 
 
     const fetchUsers = (search) =>{
-        // firebase.firestore()
-        // .collection("users")
-        // .where("name" , ">=" , search)
-        // .get()
-        // .then((snapshot)=>{
-        //     let users = snapshot.docs.map((doc)=>{
-        //         const data = doc.data()
-        //         const id = doc.id
+        firebase.firestore()
+        .collection("users")
+        .where("name" , ">=" , search)
+        .get()
+        .then((snapshot)=>{
+            let users = snapshot.docs.map((doc)=>{
+                const data = doc.data()
+                const id = doc.id
 
-        //         return({id, ...data})
+                return({id, ...data})
 
-        //     })
+            })
 
 
-        //     setUsers(users)
-        // })
-        console.log("fetch users")
+            setUsers(users)
+        })
     }
     return (
      <View>
