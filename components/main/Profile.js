1e8aeb283d67d44bf178364801bf122a254046c1
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-16 19:57:45 +0100

    --- Auto-Git Commit ---

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,94 +1,85 @@
-import { Text, View } from 'native-base'
-import React, { Component } from 'react'
-import { StyleSheet, FlatList, Image } from 'react-native'
-import { connect, bindActionCreators } from 'react-redux'
-import {fetchUser, fetchUserPosts}  from "../../redux/actions/index"
- class Profile extends Component {
-     constructor(props){
-         super(props)
-     }
+import { Text, View } from "native-base";
+import React, { Component } from "react";
+import { StyleSheet, FlatList, Image } from "react-native";
+import { connect, bindActionCreators } from "react-redux";
+import { fetchUser, fetchUserPosts } from "../../redux/actions/index";
+class Profile extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-     componentDidMount(){
+  componentDidMount() {
+    const { currentUser, posts } = this.props;
+  }
 
-        const {currentUser, posts} = this.props
-     
+  renderItems = (items) => {
+    // console.log("rendering items!!!!!!!!!!!!!", items.downloadURL);
 
-     }
-     
+    return (
+      <Image
+        source={{
+          uri: items.downloadURL,
+        }}
+        style={styles.image}
+      />
+    );
+  };
+  render() {
+    const { currentUser, posts } = this.props;
+    // console.log({currentUser, posts})
+    return (
+      <View style={styles.container}>
+        <View style={styles.containerInfo}>
+          <Text> {currentUser && currentUser.name}</Text>
+          <Text> {currentUser && currentUser.email}</Text>
+        </View>
 
-
-     renderItems =(items) => {
-         console.log("rendering items!!!!!!!!!!!!!" ,items.downloadURL)
-
-        return(
-            <Image 
-            source =  {{
-                uri : items.downloadURL
-            }}
-    
-            style = {styles.image}
+        <View style={styles.containerGallery}>
+          <View style={styles.containerImage}>
+            <FlatList
+              numColumns={3}
+              horizontal={false}
+              data={posts}
+              renderItem={({ item }) => {
+                this.renderItems(item);
+              }}
             />
-        )
-       
-     }
-    render() {
-        const {currentUser, posts} = this.props
-        // console.log({currentUser, posts})
-        return (
-          <View style = {styles.container}>
-                {/* <View style = {styles.containerInfo}>
-                <Text> {currentUser && currentUser.name}</Text>
-                <Text> {currentUser && currentUser.email}</Text>
-            </View> */}
-
-            <View style = {styles.containerGallery}>
-
-                <FlatList numColumns = {3}
-                    horizontal = {false}
-
-                    data = {posts}
-                    renderItem = {({item}) =>{
-                        this.renderItems(item)
-                    }}
-                />
-
-            </View>
           </View>
-        )
-    }
+        </View>
+      </View>
+    );
+  }
 }
 
-
-
-
 const styles = StyleSheet.create({
-    container : {
-        flex :1,
-        marginTop : 40
-        // alignContent : "center",
-        // justifyContent : "center"
-    },
-    containerInfo : {
-      margin : 20
-    },
-    containerGallery : {
-    //  flex : 1,
-    margin : 10
-    },
-    image : {
-        flex : 1,
-        aspectRatio : 1/1
-    }
-})
+  container: {
+    flex: 1,
+    marginTop: 40,
+    // alignContent : "center",
+    // justifyContent : "center"
+  },
+  containerInfo: {
+    margin: 20,
+  },
+  containerGallery: {
+    flex: 1,
+  },
+  containerImage: {
+    flex: 1 / 3,
+  },
+  image: {
+    flex: 1,
+    aspectRatio: 1 / 1,
+  },
+});
 
 // const mapDispatchToProps = (dispatch) => bindActionCreators({
 //     fetchUser, fetchUserPosts
 // }, dispatch)
 
-const mapStateToProps =(store)=>({
-    currentUser : store.userState.currentUser,
-    posts : store.userState.posts
-    
-})
+const mapStateToProps = (store) => ({
+  currentUser: store.userState.currentUser,
+  posts: store.userState.posts,
+});
 
-export default connect( mapStateToProps, null)(Profile)
+export default connect(mapStateToProps, null)(Profile);
