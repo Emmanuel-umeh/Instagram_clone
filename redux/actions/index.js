641dc763596b5c60d363a794ae94b697954dd6ee
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-31 19:47:54 +0100

    --- Auto-Git Commit ---

--- redux/actions/index.js
+++ redux/actions/index.js
@@ -140,6 +140,8 @@ export function fetchUsersFollowingPosts(uid) {
         const uid = snapshot.query.EP.path.segments[1]
         console.log({snapshot, uid})
    
+        const user = getState().usersState.users.find(el =>el.uid === uid)
+
           let posts = snapshot.docs.map((doc)=>{
               const data = doc.data()
               const id = doc.id;
