806eb3f765cf08df470795c4414bdfd1daa702f6
Emmanuel Umeh <emmanuelsumeh@gmail.com>
2021-03-31 19:20:38 +0100

    --- Auto-Git Commit ---

--- redux/reducers/users.js
+++ redux/reducers/users.js
@@ -1,1 +1,35 @@
 // reducer for all the users on the platform
+
+import { USER_FOLLOWING_STATE_CHANGE, USER_POST_STATE_CHANGED, USER_STATE_CHANGED } from "../constants";
+
+const initialState = {
+  currentUser: null,
+  following : [],
+  // my personal posts
+  posts: [],
+};
+
+export const user = (state = initialState, actions) => {
+  switch (actions.type) {
+    case USER_STATE_CHANGED:
+      return {
+        ...state,
+        currentUser: actions.currentUser,
+      };
+    case USER_POST_STATE_CHANGED:
+      return {
+        ...state,
+        posts: actions.posts,
+      };
+    case USER_FOLLOWING_STATE_CHANGE:
+
+    console.log( actions.following)
+      return {
+        ...state,
+        following: actions.following,
+      };
+
+    default:
+      return state;
+  }
+};
